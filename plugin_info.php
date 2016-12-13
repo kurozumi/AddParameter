be1965f225c1f625e44f218fa8da382b967e0552
kurozumi <kurozumi@internetmarketing.co.jp>
2016-12-13 11:05:37 +0900

    README更新

--- plugin_info.php
+++ plugin_info.php
@@ -14,7 +14,7 @@ class plugin_info {
     /** プラグインコード(必須)：システム上でのキーとなります。プラグインコードは一意である必要があります。 */
     static $PLUGIN_CODE        = "AddParameter";
     /** プラグイン名(必須)：プラグイン管理・画面出力（エラーメッセージetc）にはこの値が出力されます。 */
-    static $PLUGIN_NAME        = "管理画面パラメータ追加プラグイン";
+    static $PLUGIN_NAME        = "パラメータ追加プラグイン";
     /** プラグインメインクラス名(必須)：本体がプラグインを実行する際に呼ばれるクラス。拡張子は不要です。 */
     static $CLASS_NAME         = "AddParameter";
     /** プラグインバージョン(必須) */
@@ -24,7 +24,7 @@ class plugin_info {
     /** 作者(必須) */
     static $AUTHOR             = "kurozumi";
     /** 説明(必須) */
-    static $DESCRIPTION        = "管理画面のパラメータが追加できます。";
+    static $DESCRIPTION        = "パラメータ設定のパラメータが追加できます。";
     /** 作者用のサイトURL：設定されている場合はプラグイン管理画面の作者名がリンクになります。 */
     static $AUTHOR_SITE_URL    = "";
     /** プラグインのサイトURL : 設定されている場合はプラグイン管理画面の作者名がリンクになります。 */
